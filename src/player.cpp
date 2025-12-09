c683eae8a0b3e07400ab928c955eabe7e8dc9149
Julie Bonne <juliebonne@MacBook-Pro-de-Julie.local>
2025-12-09 18:23:38 +0100

    Modification de player.cpp

--- src/player.cpp
+++ src/player.cpp
@@ -1,3 +1,12 @@
 #include <iostream>
 #include "player.hpp"
 
+player create_player (){
+    player p;
+    std::cout << "Entrez le nom du joueur : ";
+    std::cin >> p.name;
+    std::cout << "Choisissez un symbole (X ou O) : ";
+    std::cin >> p.symbol;
+    
+    return p;
+}
